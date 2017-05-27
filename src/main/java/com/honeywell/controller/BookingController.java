35c2ef0b4ed8308180afed6906e5cc1457d47d20
Jayapala Manjegowda <jayapala.manjegowda@radisys.com>
2017-05-27 12:25:05 +0530

    booking service impl

--- src/main/java/com/honeywell/controller/BookingController.java
+++ src/main/java/com/honeywell/controller/BookingController.java
@@ -1,8 +1,27 @@
 package com.honeywell.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.honeywell.model.BookingDetails;
+import com.honeywell.service.BookingService;
+import org.apache.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/service")
 public class BookingController {
-//TODO
+
+    @Autowired
+    BookingService bookingService;
+
+    @RequestMapping(value = "/book/", method = RequestMethod.POST)
+    public void bookSeats(@RequestBody BookingDetails bookingDetails) {
+         bookingService.book(bookingDetails);
+    }
+
+    @RequestMapping(value = "/book/", method = RequestMethod.POST)
+    public void getSeats(@RequestParam int id) {
+        bookingService.getSeats(id);
+    }
+
 }
--- src/main/java/com/honeywell/service/BookingService.java
+++ src/main/java/com/honeywell/service/BookingService.java
@@ -1,10 +1,16 @@
 package com.honeywell.service;
 
+import com.honeywell.model.BookingDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookingService {
 
-    //todu jp
+    public void book(BookingDetails bookingDetails) {
 
+    }
+
+    public int getSeats(int id ) {
+        return 1;
+    }
 }
